95143b7eaa092df1a34a0d3360f3a61a0cef1a6a
Nguyen Thien An <21120192@student.hcmus.edu.vn>
2024-08-04 17:30:35 +0700

    Add 1 new solution

---
+++ IntroductionToCode/Chapter07/SumMatrix/TASK01/Task01.cpp
@@ -0,0 +1,174 @@
+#include <stdio.h>
+#include <string.h>
+
+#define TOI_DA 100
+typedef double MANG2D_THUC[TOI_DA][TOI_DA];
+
+void nhapMaTranThuc(MANG2D_THUC maTran, unsigned int& soDong, unsigned int& soCot);
+void xuatMaTranThuc(MANG2D_THUC maTran, unsigned int soDong, unsigned int soCot);
+bool kiemTraGiaTriLaSoNguyenDuong(char giaTri[]);
+bool kiemTraGiaTriLaSoThuc(char giaTri[]);
+unsigned int chuyenChuoiThanhSoNguyenDuong(char giaTri[]);
+double chuyenChuoiThanhSoThuc(char giaTri[]);
+void tinhTongDongTrenMaTran(MANG2D_THUC maTran, unsigned int soDong, unsigned int soCot, double mangTong[]);
+void xuatMangThuc(double mangThuc[], unsigned int soPhanTu);
+
+int main()
+{
+	MANG2D_THUC maTran{ 0 };
+	unsigned int soDong = 0, soCot = 0;
+	double mangTong[TOI_DA]{ 0 };
+
+	nhapMaTranThuc(maTran, soDong, soCot);
+
+	printf("Ma tran da nhap la:\n");
+	xuatMaTranThuc(maTran, soDong, soCot);
+
+	tinhTongDongTrenMaTran(maTran, soDong, soCot, mangTong);
+
+	printf("Tong moi dong trong ma tran la: ");
+	xuatMangThuc(mangTong, soDong);
+
+	return 0;
+}
+
+void nhapMaTranThuc(MANG2D_THUC maTran, unsigned int& soDong, unsigned int& soCot)
+{
+	char giaTri[20 + 1] = "";
+
+	printf("Nhap so dong cua ma tran: ");
+	scanf_s("%s", &giaTri, 20);
+	while (!kiemTraGiaTriLaSoNguyenDuong(giaTri))
+	{
+		printf("Gia tri khong hop le. Vui long nhap so nguyen duong: ");
+		scanf_s("%s", &giaTri, 20);
+	}
+	soDong = chuyenChuoiThanhSoNguyenDuong(giaTri);
+
+	printf("Nhap so cot cua ma tran: ");
+	scanf_s("%s", &giaTri, 20);
+	while (!kiemTraGiaTriLaSoNguyenDuong(giaTri))
+	{
+		printf("Gia tri khong hop le. Vui long nhap so nguyen duong: ");
+		scanf_s("%s", &giaTri, 20);
+	}
+	soCot = chuyenChuoiThanhSoNguyenDuong(giaTri);
+
+	printf("Nhap noi dung ma tran:\n");
+
+	for (unsigned int i = 0; i < soDong; i++)
+		for (unsigned int j = 0; j < soCot; j++)
+		{
+			printf("Phan tu [%d][%d]: ", i, j);
+			scanf_s("%s", &giaTri, 20);
+			while (!kiemTraGiaTriLaSoThuc(giaTri))
+			{
+				printf("Gia tri khong hop le. Vui long nhap so thuc: ");
+				scanf_s("%s", &giaTri, 20);
+			}
+			maTran[i][j] = chuyenChuoiThanhSoThuc(giaTri);
+		}
+}
+
+void xuatMaTranThuc(MANG2D_THUC maTran, unsigned int soDong, unsigned int soCot)
+{
+	for (unsigned int i = 0; i < soDong; i++)
+	{
+		for (unsigned int j = 0; j < soCot; j++)
+			printf("\t%lf", maTran[i][j]);
+
+		printf("\n");
+	}
+
+}
+
+bool kiemTraGiaTriLaSoNguyenDuong(char giaTri[])
+{
+	for (unsigned int i = 0; i < strlen(giaTri); i++)
+		if (giaTri[i] < '0' || giaTri[i] > '9')
+			return false;
+
+	return true;
+}
+
+bool kiemTraGiaTriLaSoThuc(char giaTri[])
+{
+	bool laPhanThapPhan = false;
+	for (unsigned int i = 0; i < strlen(giaTri); i++)
+		if (giaTri[i] == '-')
+		{
+			if (i != 0)
+				return false;
+		}
+		else if (giaTri[i] == '.')
+			if (i == 0 || laPhanThapPhan)
+				return false;
+			else
+				laPhanThapPhan = true;
+		else if (giaTri[i] < '0' || giaTri[i] > '9')
+			return false;
+
+	return true;
+}
+
+unsigned int chuyenChuoiThanhSoNguyenDuong(char giaTri[])
+{
+	unsigned int so = 0;
+
+	for (unsigned int i = 0; i < strlen(giaTri); i++)
+	{
+		so *= 10;
+		so += (giaTri[i] - '0');
+	}
+
+	return so;
+}
+
+double chuyenChuoiThanhSoThuc(char giaTri[])
+{
+	double so = 0, soThapPhan = 0;
+	unsigned int hangThapPhan = 10, i = 0;
+	bool laSoAm = false;
+
+	for (i = 0; i < strlen(giaTri) && giaTri[i] != '.'; i++)
+	{
+		if (giaTri[i] == '-')
+		{
+			laSoAm = true;
+			continue;
+		}
+		so *= 10;
+		so += (giaTri[i] - '0');
+	}
+
+	for (i; i < strlen(giaTri); i++)
+	{
+		if (giaTri[i] == '.')
+			continue;
+		soThapPhan = (double)(giaTri[i] - '0') / hangThapPhan;
+		so += soThapPhan;
+		hangThapPhan *= 10;
+	}
+
+	if (laSoAm) so *= -1;
+
+	return so;
+}
+
+void tinhTongDongTrenMaTran(MANG2D_THUC maTran, unsigned int soDong, unsigned int soCot, double mangTong[])
+{
+	for (unsigned int i = 0; i < soDong; i++)
+		for (unsigned int j = 0; j < soCot; j++)
+		{
+			if (j == 0)
+				mangTong[i] = 0;
+
+			mangTong[i] += maTran[i][j];
+		}
+}
+
+void xuatMangThuc(double mangThuc[], unsigned int soPhanTu)
+{
+	for (unsigned int i = 0; i < soPhanTu; i++)
+		printf("\t%lf", mangThuc[i]);
+}
